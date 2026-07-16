e43c41c51ff24d5244ca18ce32d74c3d27ff9a12
Abdulhamid <abdulhamidkuchinskiy@gmail.com>
2026-07-17 00:38:35 +0500

    odam charchasa yotib uxlash kere

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import  ProductViewSet
+from .views import  ProductViewSet,ProductCreateAPIView
 
 urlpatterns = [
     path('', ProductViewSet.as_view({'get': 'list', 'post': 'create'}), name='product-list'),
+    path('create/', ProductCreateAPIView.as_view(), name='product-create'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,11 @@
 from rest_framework import viewsets
-from .serializers import ProductSerializers
+from .serializers import ProductSerializers,CreateAPIView
 from .models import Product
 
 class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializers
+
+class ProductCreateAPIView(viewsets.CreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializers

7fb4357c853720db306938ee1e92df4d07277754
Abdulhamid <abdulhamidkuchinskiy@gmail.com>
2026-07-24 02:34:14 +0500

    prosta

--- products/models.py
+++ products/models.py
@@ -1,10 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.text import slugify
+
 
 class Product(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.PositiveIntegerField()
-    description = models.TextField(blank=True, null=True)
-    stock = models.IntegerField(default=0)  
+    name = models.CharField(max_length=100)
+    price = models.PositiveIntegerField(null=False, default=0)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=15, blank=False, null=True)
+    age = models.PositiveIntegerField(default=18, blank=True)
+    bio = models.TextField()
+    address = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateField(auto_now_add=True)
+    slug = models.SlugField(blank=True)
+
+    REQUIRED_FIELDS = ['email', 'phone_number', 'age']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.username)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.username
--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,21 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from .models import Product
 
-class ProductSerializers(serializers.ModelSerializer):
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
-        model = Product
-        fields = ["id", "name", "price", "description", "stock"]
+        model = User
+        fields = ["id", "email", "full_name", "password"]
+        read_only_fields = ["id"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data["email"],
+            password=validated_data["password"],
+            full_name=validated_data.get("full_name", ""),
+        )
+        return user
